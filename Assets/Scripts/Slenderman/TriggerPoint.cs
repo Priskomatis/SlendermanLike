1e46cbeaf4945726a2fb70aab26cf019e78f4421
Priskomatis <I.Priskomatis@acg.edu>
2023-12-03 16:18:39 +0200

    Added documentation to slenderman scripts

--- Assets/Scripts/Slenderman/TriggerPoint.cs
+++ Assets/Scripts/Slenderman/TriggerPoint.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class TriggerPoint : MonoBehaviour
 {
+
+    /**
+     * 
+     * TriggerPoint class is a script for the sole purpose of the generation of "Jump Scare" event system;
+     * Basic methodology:
+     * Once the player enters a trigger area, the Slenderman prefab game object will be instantiated in a spawn point location;
+     * This script takes the following parameters:
+     * -Transform slenderSpawnPoint: The location where the prefab Slenderman game object will be instantiated in the game world;
+     * -Camera camera: The main camera of the game, this should located on the player game object;
+     * -GameObject slenderman: The prefab gameObject of the Slenderman that gets instantiated;
+     * 
+     */
     [SerializeField] private Camera camera;
 
     [SerializeField] private Transform slenderSpawnPoint;
@@ -11,6 +23,7 @@ public class TriggerPoint : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // If the player steps inside the trigger area that we set;
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player here!");
@@ -18,15 +31,18 @@ public class TriggerPoint : MonoBehaviour
             // Instantiate slenderman and store the reference
             GameObject instantiatedSlenderman = Instantiate(slenderman, slenderSpawnPoint.position, Quaternion.Euler(0f, 180, 0f));
 
-            // Start coroutines for hiding slenderman and the TriggerPoint GameObject
+            // Start coroutines for hiding slenderman and the TriggerPoint GameObject;
+            //This makes to so we disable the slenderman game object after fixed amount of time has passed;
             StartCoroutine(HideSlender(instantiatedSlenderman));
         }
     }
 
     private IEnumerator HideSlender(GameObject slendermanToHide)
     {
+        // Wait 1f seconds before continuing;
         yield return new WaitForSeconds(1f);
         
+        //Call the function to check when to disable slenderman prefab;
         if(IsVisible(camera, slendermanToHide))
         {
             slendermanToHide.SetActive(false);
@@ -36,8 +52,10 @@ public class TriggerPoint : MonoBehaviour
         gameObject.SetActive(false);
     }
 
+    // Function to check if slenderman is visible in the camera view
     private bool IsVisible(Camera c, GameObject target)
     {
+        //Calculates frustum planes by taking the camer's views and returns 6 planes that form it;
         var planes = GeometryUtility.CalculateFrustumPlanes(c);
         var point = target.transform.position;
 
